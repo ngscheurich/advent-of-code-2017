a9efdd7ddcad3987be42033d35109bcb8ab27c7b
N. G. Scheurich <nick@scheurich.me>
2017-12-15 14:13:04 -0600

    Fix Day Two test

--- day_two/test/day_two_test.exs
+++ day_two/test/day_two_test.exs
@@ -4,11 +4,11 @@ defmodule DayTwoTest do
 
   test "calculates the correct checksum" do
     input = """
-    5 1 9 5
-    7 5 3
-    2 4 6 8
+    5\t1\t9\t5
+    7\t5\t3
+    2\t4\t6\t8
     """
 
-    assert DayTwo.checksum(input) == 18
+    assert DayTwo.solve(input) == 18
   end
 end
